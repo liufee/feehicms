874c394556eddf4497eac57a82fa96974bf2207d
lf <job@feehi.com>
2018-01-17 22:52:34 +0800

    fix bug

---
+++ common/helpers/Util.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: lf
+ * Blog: https://blog.feehi.com
+ * Email: job@feehi.com
+ * Created at: 2018-01-17 22:46
+ */
+
+namespace common\helpers;
+
+
+class Util extends \cms\common\helpers\Util
+{
+
+}
